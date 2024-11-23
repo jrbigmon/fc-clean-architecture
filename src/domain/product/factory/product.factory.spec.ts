492cb4906420590ac15013eb43bd6628b547bb79
jrbigmon <juniorterriaga0@gmail.com>
2024-11-23 09:19:06 -0300

    test: adjusted enum to create a product for each type of product

--- src/domain/product/factory/product.factory.spec.ts
+++ src/domain/product/factory/product.factory.spec.ts
@@ -1,8 +1,8 @@
-import ProductFactory from "./product.factory";
+import ProductFactory, { TypeOfProduct } from "./product.factory";
 
 describe("Product factory unit test", () => {
   it("should create a proct type a", () => {
-    const product = ProductFactory.create("a", "Product A", 1);
+    const product = ProductFactory.create(TypeOfProduct.A, "Product A", 1);
 
     expect(product.id).toBeDefined();
     expect(product.name).toBe("Product A");
@@ -11,7 +11,7 @@ describe("Product factory unit test", () => {
   });
 
   it("should create a proct type b", () => {
-    const product = ProductFactory.create("b", "Product B", 1);
+    const product = ProductFactory.create(TypeOfProduct.B, "Product B", 1);
 
     expect(product.id).toBeDefined();
     expect(product.name).toBe("Product B");
@@ -20,8 +20,8 @@ describe("Product factory unit test", () => {
   });
 
   it("should throw an error when product type is not supported", () => {
-    expect(() => ProductFactory.create("c", "Product C", 1)).toThrowError(
-      "Product type not supported"
-    );
+    expect(() =>
+      ProductFactory.create("c" as TypeOfProduct, "Product C", 1)
+    ).toThrowError("Product type not supported");
   });
 });
